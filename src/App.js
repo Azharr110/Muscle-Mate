dfb320b9a3152ef74c6e931d14114e92ab23ae87
Azhar <abdazhar786@gmail.com>
2023-04-06 01:23:34 +0530

    Add Starting page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import {Box} from '@mui/material'
 
+import '../src/App.css';
 import ExerciseDetail from './pages/ExerciseDetail'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'

f427c531cd6a36be492746ac7eb078dd0d26eade
Azhar <abdazhar786@gmail.com>
2023-04-09 03:00:45 +0530

    Add key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,14 +3,14 @@ export const exerciseOptions = {
     method: 'GET',
     url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
-      'X-RapidAPI-Key': '248b61cec8msh8c332763162eabap11a6ffjsn5ed56ecac2a6',
-    //   'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+     
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
         }
-      
-}
+      }
 
-export const fetchData = async (url, options) =>{
+      console.log(process.env.REACT_APP_RAPID_API_KEY)
+        export const fetchData = async (url, options) =>{
     const response = await fetch(url, options)
     const data = await response.json()
 

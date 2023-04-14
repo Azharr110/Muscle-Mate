0b972216d22962742903e775f6d66f536f037d1f
Azhar <abdazhar786@gmail.com>
2023-04-14 06:43:52 +0530

    Add show exercise on clicking icon

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -21,6 +21,22 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
 
   }
   // console.log(exercises)
+
+  useEffect(()=> {
+    const fetchExercisesData = async () => {
+      let exercisesData = []
+
+      if(bodyPart === 'all'){
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions)
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+
+      setExercises(exercisesData);
+    }
+
+    fetchExercisesData()
+  },[bodyPart])
   return (
     <Box id="exercises"
         sx={{ mt: {lg: '110px'}}}

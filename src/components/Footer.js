fcd67e85ceccb0b6a86be49a7d61897ea0a3ee9c
Azhar <abdazhar786@gmail.com>
2023-04-18 23:55:21 +0530

    Add Loader

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
       <img src={Logo} alt="logo" style={{ width: '48px', height: '48px' , margin:'0 20px'}} />
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Get Fit and Strong with <span style={{ textTransform: 'capitalize' }}>Muscle Mate</span></Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Get Fit and Strong with <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Muscle Mate</span></Typography>
   </Box>
 );
 

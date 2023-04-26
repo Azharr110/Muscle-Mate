5c27d91bafa9e5788aae132dc3ae09f661808dfa
Azhar <abdazhar786@gmail.com>
2023-04-26 20:03:04 +0530

    remove carousel img

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -12,7 +12,7 @@ const LeftArrow = () => {
 
   return (
     <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
+      {/* <img src={LeftArrowIcon} alt="right-arrow" /> */}
     </Typography>
   );
 };
@@ -22,7 +22,7 @@ const RightArrow = () => {
 
   return (
     <Typography onClick={() => scrollNext()} className="left-arrow">
-      <img src={RightArrowIcon} alt="right-arrow" />
+      {/* <img src={RightArrowIcon} alt="right-arrow" /> */}
     </Typography>
   );
 };

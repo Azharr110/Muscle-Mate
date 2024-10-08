96258900db2aa4364d5a5d84977d1c9e82dfaa47
Azharr110 <abdazhar786@gmail.com>
2024-10-09 00:22:01 +0530

    add data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,21 +7,19 @@ export const exerciseOptions = {
     //   'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     //   'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     //     }
-      
 
     method: 'GET',
     url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
-      'X-RapidAPI-Key': '9c91cca313msh153546c38874335p132748jsnddd8f9826fb5',
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+ 
+      'X-RapidAPI-Key': 'b9b895c497msha7162157c60136dp1f7c45jsnb043f24603c0', // At, az786, az7866, az110
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+
     }
   }  
-
       export const youtubeOptions = {
         method: 'GET',
-      
         headers: {
-          
           'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
           'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
         }
@@ -31,6 +29,7 @@ export const exerciseOptions = {
         export const fetchData = async (url, options) =>{
     const response = await fetch(url, options)
     const data = await response.json()
+    // console.log(data);
 
     return data
 } 

53c844d725f617b52f03aebaac872189cae24c46
Azharr110 <abdazhar786@gmail.com>
2024-10-10 18:40:26 +0530

    remove arrow

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -4,8 +4,8 @@ import { Box, Typography} from '@mui/material';
 
 import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
-import RightArrowIcon from '../assets/icons/right-arrow.png';
-import LeftArrowIcon from '../assets/icons/left-arrow.png';
+// import RightArrowIcon from '../assets/icons/right-arrow.png';
+// import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
